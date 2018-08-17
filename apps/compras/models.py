0dd56165c5932a7ce00b3f0733cfccb43852de3a
lescurra <luupaez@gmail.com>
2018-08-17 17:13:45 -0400

    modelo de compras

--- apps/compras/models.py
+++ apps/compras/models.py
@@ -1,3 +1,50 @@
 from django.db import models
+from apps.proveedores.models import Proveedor
 
-# Create your models here.
+
+# Orden de Compra Cabecera
+class OrdenCompraCab(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    fecha = models.DateTimeField(auto_now_add=True)
+    proveedor = models.ForeignKey(Proveedor, on_delete=models.PROTECT, blank=True, null=True)
+    PENDIENTE = 'PENDIENTE'
+    CONFIRMADO = 'CONFIRMADO'
+    FACTURADO = 'FACTURADO'
+    ESTADO_CHOICES = (
+        (PENDIENTE, 'Pendiente'),
+        (CONFIRMADO, 'Confirmado'),
+        (FACTURADO, 'Facturado'),
+    )
+    estado = models.CharField(max_length=20, choices=ESTADO_CHOICES, default=PENDIENTE)
+
+    def __str__(self):
+        return self.id
+
+    def as_json(self):
+        return dict(
+            id=self.id)
+
+    class Meta:
+        """Establece las configuraciones del modelo de base de datos"""
+        managed = True
+        db_table = 'orden_compra_cabecera'
+
+
+# Orden de Compra Detalle
+class OrdenCompraDet(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    compra_cab = models.ForeignKey(OrdenCompraCab, on_delete=models.PROTECT, blank=True, null= False)
+    producto = models.ForeignKey(Proveedor, on_delete=models.PROTECT, blank=True, null=True)
+    cantidad = models.IntegerField(blank=True, null=True)
+
+    def __str__(self):
+        return self.id
+
+    def as_json(self):
+        return dict(
+            id=self.id)
+
+    class Meta:
+        """Establece las configuraciones del modelo de base de datos"""
+        managed = True
+        db_table = 'orden_compra_detalle'
