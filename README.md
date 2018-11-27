308df8c7e448268ab2f52412a7a449ba4fed9cf5
Willian Toledo <wtoledo@roshka.com>
2018-11-27 20:00:06 -0300

    Se agrega la documentacion para levantar el proyecto

---
+++ README.md
@@ -0,0 +1,23 @@
+##CREAR AMBIENTE
+
+Instalar Django  
+https://tutorial.djangogirls.org/en/installation/
+
+##BASE DE DATOS
+
+1. INSTALAR pypgscop
+
+2. CREAR LA BASE DE DATOS
+
+3. EJECUTAR LOS MIGRATIONS
+python manage.py makemigrations
+python manage.py migrate
+
+4. Ejecutar los scripts sql
+
+## CREAR SUPERUSUARIO
+
+python manage.py createsuperuser
+
+## EJECUTAR
+python manage.py runserver
---
+++ scripts/crear_tablas.sql
@@ -0,0 +1,22 @@
+insert into producto_tipo (descripcion,fecha_hora_creacion) values('LUBRICANTE', current_timestamp)
+insert into producto_tipo (descripcion,fecha_hora_creacion) values('REPUESTO', current_timestamp)
+insert into producto_tipo (descripcion,fecha_hora_creacion) values('SERVICIO', current_timestamp)
+ INSERT INTO public.marca(descripcion) VALUES ('TOYOTA');
+INSERT INTO public.marca(descripcion) VALUES ('NISSAN');
+INSERT INTO public.marca(descripcion) VALUES ('SUZUKI');
+INSERT INTO public.marca(descripcion) VALUES ('ISUZU');
+INSERT INTO public.marca(descripcion) VALUES ('CHEVROLET');
+INSERT INTO public.marca(descripcion) VALUES ('KIA');
+
+
+INSERT INTO public.modelo(descripcion, marca_id) VALUES ('HILUX', 1);
+INSERT INTO public.modelo(descripcion, marca_id) VALUES ('VITZ', 1);
+INSERT INTO public.modelo(descripcion, marca_id) VALUES ('AURIS', 1);
+INSERT INTO public.modelo(descripcion, marca_id) VALUES ('COROLLA', 1);
+INSERT INTO public.modelo(descripcion, marca_id) VALUES ('ETIOS', 1);
+INSERT INTO public.modelo(descripcion, marca_id) VALUES ('LAND CRUISER', 1);
+INSERT INTO public.modelo(descripcion, marca_id) VALUES ('PLATZ', 1);
+INSERT INTO public.modelo(descripcion, marca_id) VALUES ('PRIUS', 1);
+INSERT INTO public.modelo(descripcion, marca_id) VALUES ('RAV4', 1);
+ 
+
